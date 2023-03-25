20589b8ea8f5ea840b6ed0902da7f1d8e4c6e1de
Adam Meadows <adam.meadows@gmail.com>
2023-03-25 10:20:43 -0600

    Working zsh init script

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,37 +1,22 @@
-#! /bin/sh
-
-#
-# Courtesy of https://oliverspryn.blog/adding-git-completion-to-zsh-60f3b0e7ffbc
-#
-
 # Create initial .zsh/ dir
 ZDIR=~/.zsh
+ZDIR_STR='~/.zsh'
 mkdir -p $ZDIR
 
 # Add git completion
-cd $ZDIR
-curl -o git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-curl -o _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+curl -o $ZDIR/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o $ZDIR/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 # Add spaceship-prompt
 brew install spaceship
 
-ENV=~/.zsh/env.zsh
+ENV=$ZDIR/env.zsh
 echo '#' >> $ENV
 echo '# Machine-specific environment variables go here' >> $ENV
 echo '#' >> $ENV
 echo 'export NPM_TOKEN=""' >> $ENV
 
-VNODE_COMP=$ZDIR/_vnode
-echo '#compdef vnode' > $VNODE_COMP
-echo '' > $VNODE_COMP
-echo '_vnode() {' > $VNODE_COMP
-echo '  _files -/ -W ${NODE_ENV_DIR}' > $VNODE_COMP
-echo '}' > $VNODE_COMP
-echo '' > $VNODE_COMP
-echo '_vnode' > $VNODE_COMP
-
-CONF=~/.zsh/configs.zsh
+CONF=$ZDIR/configs.zsh
 echo '#' >> $CONF
 echo '# Common configs from dotfiles' >> $CONF
 echo '#' >> $CONF
@@ -44,16 +29,29 @@ echo 'do' >> $CONF
 echo '  source ${CONFIGS}/${file}.zsh' >> $CONF
 echo 'done' >> $CONF
 
+VNODE_COMP=$ZDIR/_vnode
+echo '#compdef vnode' > $VNODE_COMP
+echo '' > $VNODE_COMP
+echo '_vnode() {' > $VNODE_COMP
+echo '  _files -/ -W ${NODE_ENV_DIR}' > $VNODE_COMP
+echo '}' > $VNODE_COMP
+echo '' > $VNODE_COMP
+echo '_vnode' > $VNODE_COMP
+
 RC=~/.zshrc
+RC_STR='~/.zshrc'
 echo '# Load Git completion (and my custom configs)' >> $RC
-echo "zstyle ':completion:*:*:git:*' script ~/$ZDIR/git-completion.bash" >> $RC
-echo "fpath=($ZDIR $$fpath)" >> $RC
+echo "zstyle ':completion:*:*:git:*' script ~/$ZDIR_STR/git-completion.bash" >> $RC
+echo "fpath=($ZDIR_STR \$fpath)" >> $RC
 echo '' >> $RC
 echo 'autoload -Uz compinit && compinit' >> $RC
 echo '' >> $RC
-echo "source $ZDIR/env.zsh" >> $RC
-echo "source $ZDIR/configs.zsh" >> $RC
+echo "source $ZDIR_STR/env.zsh" >> $RC
+echo "source $ZDIR_STR/configs.zsh" >> $RC
 echo '' >> $RC
 echo 'source $(brew --prefix)/opt/spaceship/spaceship.zsh' >> $RC
 
-echo "source $RC" >> ~/.zprofile
+PROFILE=~/.zprofile
+echo "source $RC_STR" >> $PROFILE
+
+source $PROFILE

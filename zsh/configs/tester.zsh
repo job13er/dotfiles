9fa71e82bb65dadc9ac8a93d1403e2505b790be1
Adam Meadows <adam.meadows@gmail.com>
2024-04-10 08:14:41 -0600

    Add tester test helpers

---
+++ zsh/configs/tester.zsh
@@ -0,0 +1,32 @@
+#
+# Helpers to simplify running web tester tests
+#
+#
+
+function get_module() {
+    full_path=$1
+    filename=$full_path:t
+    dir=$full_path:h
+    module="${filename%.*}"
+    full_module="$(echo $dir | tr / .).${module}"
+    echo $full_module
+}
+
+function test_browser() {
+    browser=$1
+    full_module=$(get_module $2)
+
+    echo CARIUM_TESTER_DEFAULT_STOP=5 tester run --driver=${browser} --test=${full_module}
+    CARIUM_TESTER_DEFAULT_STOP=5 tester run --driver=${browser} --test=${full_module}
+}
+
+function test_chrome() {
+    test_browser chrome $1
+}
+
+function test_firefox() {
+    test_browser firefox $1
+}
+
+alias tchrome="test_chrome"
+alias tff="test_firefox"
